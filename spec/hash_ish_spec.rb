8f296b8ed905e9ca7bfcc908d6725ea834fe1b2d
Michael Pope <mpope.cr@gmail.com>
2015-11-11 22:40:03 -0500

    Initialize Rspec

--- spec/hash_ish_spec.rb
+++ spec/hash_ish_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 
 describe HashIsh do
   it 'has a version number' do
-    expect(HashIsh::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(HashIsh::VERSION).not_to eq(nil)
   end
 end
