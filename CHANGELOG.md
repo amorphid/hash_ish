f625915f51fe9cb97ece2a80e1ba0f91bf6c71e3
Michael Pope <mpope.cr@gmail.com>
2015-11-12 08:16:46 -0500

    Add HashIsh.new now returns a subclass of Hash
    - Also removed pre-built gem from main directory

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## 0.2.0 (2015-07-22)
+
+Features:
+  - HashIsh.new now object that descends from Hash
--- lib/hash_ish.rb
+++ lib/hash_ish.rb
@@ -4,16 +4,20 @@ require 'hash_ish/add_instance_methods'
 class HashIsh
   def self.new(kwargs = {})
     instance(
-      decorate_klass(kwargs))
+      decorate_klass(hash_sub_class, kwargs))
+  end
+
+  def self.decorate_klass(klass, kwargs)
+    klass.tap do |klass|
+      AddInstanceMethods.new.add(klass, kwargs)
+    end
   end
 
   def self.instance(klass)
     klass.new
   end
 
-  def self.decorate_klass(kwargs)
-    Class.new.tap do |klass|
-      AddInstanceMethods.new.add(klass, kwargs)
-    end
+  def self.hash_sub_class
+    Class.new(Hash)
   end
 end
